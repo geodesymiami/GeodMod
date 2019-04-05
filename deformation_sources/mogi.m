3217c5397268080d5ae0fb748eb1640d1b71e4a2
Zhang Yunjun <yunjunz@users.noreply.github.com>
2019-04-04 23:54:04 -0400

    Mogi: update from 1998 to 2002 version
    + remove mogi_source.m as it's exactly the same as mogi.m.
    
    + update mogi.m from 1998 version to 2002 version from P. Cervelli
    
    + This update add the 4th argument, shear modulus, if omitted, default unity of 1 is used.

--- deformation_sources/mogi.m
+++ deformation_sources/mogi.m
@@ -1,75 +1,97 @@
-function [u,e,t]=Mogi(volgeom, xloc, nu)
-%Mogi    [u,e,t]=Mogi(volgeom, xloc, nu)
+function [U,D,S]=Mogi(m,xloc,nu,mu)
+%Mogi    [U,D,S]=Mogi(m,xloc,nu,mu)
 %
-%Computes surface displacements, strains, and tilts due to a Mogi source.
+%Computes displacements, strains and stresses from a point (Mogi) source.
+%Inputs m and xloc can be matrices; for multiple models, the deformation
+%fields from each are summed.
 %
 %Inputs:
-%     volgeom = Mogi source geometry: East, North, Depth, Volume change
-%               <length, length, length, volume>
-%        xloc = Matrix of local station coordinates <length>, stored columnwise,
-%               i.e., east in first row, north in second row
-%          nu = Poisson's ratio
+%    m = 4xn volume source geometry (length; length; length; length^3)
+%        (x-coord, y-coord, depth(+), volume change)
+% xloc = 3xs matrix of observation coordinates (length)
+%   nu = Poisson's ratio
+%   mu = shear modulus (if omitted, default value is unity)
 %
-%Output:
-%           u = matrix of displacements: Ux, Uy, Uz <length>
-%           e = matrix of strains: Exx, Exy, Eyy
-%           t = matrix of tilts: dUz/dx, dUz/dy
+%Outputs:
+%    U = 3xs matrix of displacements (length)
+%        (Ux,Uy,Uz)
+%    D = 9xn matrix of displacement derivatives
+%        (Dxx,Dyx,Dzx,Dxy,Dyy,Dzy,Dxz,Dyz,Dzz)
+%    S = 6xn matrix of stresses
+%        (Sxx,Sxy,Sxz,Syy,Syz,Szz)
 %
-%Notes: The term 'depth' denotes an unsigned length and should therefore always be
-%given positive.  Keep your length units consistent! If you mix km and m you may get
-%unexpected results, particularly with the strains and tilts.   
+%June 17, 1998. Peter Cervelli.
+%Revised November 3, 2000.
+%Fixed a bug ('*' multiplication should have been '.*'), August 22, 2001. Kaj Johnson
+%Fixed error in comments that had transposed the elements of D, February 6, 2002. Peter Cervelli
 %
-%10-18-00 Fixed a bug in the tilts. PFC
-%05-17-98 Peter Cervelli.
+%For information on the basis for this code see:
+%
+%Okada, Y. Internal deformation due to shear and tensile faults in a half-space,
+%    Bull. Seismol. Soc. Am., 82, 1018-1049, 1992.
 
-%Check arguments
+if nargin <4
+     mu =1;
+end
 
-	if nargin < 3 | nargin > 5 | nargout > 3
-		error('Usage: [u,e,t]=Mogi(volgeom, xloc, nu).');
-	end
+if size(xloc,1)==2
+    xloc(3,:)=0;
+end
 
-	[v]=size(volgeom);
 
-	if min(v)~=1 | max(v)~=4
-		error('First argument must be a 4 element source geometry vector.');
-	end
+%Define constant lambda
 
-	[x]=size(xloc);
-	
-	if x(1)~=2
-		error('Second argument must be a 2xn matrix of station coordinates.');
-	end
+   lambda=2*mu*nu/(1-2*nu);
+   U=zeros(3,size(xloc,2));
+   D=zeros(9,size(xloc,2));
+   S=zeros(6,size(xloc,2));
 
-	[P]=length(nu);
+%Loop over models
 
-	if P~=1
-		error('Third argument must be a scalar (Poisson''s ratio).');
-	end  	
+     for i=1:size(m,2)
 
-%Compute displacements
+          C=m(4)/(4*pi);
+          x=xloc(1,:)-m(1,i);
+          y=xloc(2,:)-m(2,i);
+          z=xloc(3,:);
+          d1=m(3,i)-z;
+          d2=m(3,i)+z;
+          R12=x.^2+y.^2+d1.^2;
+          R22=x.^2+y.^2+d2.^2;
+          R13=R12.^1.5;
+          R23=R22.^1.5;
+          R15=R12.^2.5;
+          R25=R22.^2.5;
+          R17=R12.^3.5;
+          R27=R12.^3.5;
 
-	E=volgeom(1)-xloc(1,:);
-	N=volgeom(2)-xloc(2,:);
-	E2=E.^2;
-	N2=N.^2;
-	d2=volgeom(3)^2;
-	C=(nu-1)*volgeom(4)/pi;
-	R=sqrt(d2+E2+N2);
-	R3=C*R.^-3;
-	u=[E.*R3;N.*R3;-volgeom(3)*R3];
+          %Calculate displacements
 
-%Compute strains (if necessary)
+               U(1,:) = U(1,:) + C*( (3 - 4*nu)*x./R13 + x./R23 + 6*d1.*x.*z./R15 );
+               U(2,:) = U(2,:) + C*( (3 - 4*nu)*y./R13 + y./R23 + 6*d1.*y.*z./R15 );
+               U(3,:) = U(3,:) + C*( (3 - 4*nu)*d1./R13 + d2./R23 - 2*(3*d1.^2 - R12).*z./R15);
 
-	if nargout > 1
- 		R5=C*R.^-5;
-     	e(1,:)=R5.*(2*E2-N2-d2);
-     	e(2,:)=3*R5.*(E.*N);
-     	e(3,:)=R5.*(2*N2-E2-d2);
-	end
+          %Calculate their derivatives
+          
+          	   D(1,:) = D(1,:) + C*( (3 - 4*nu)*(-3*x.^2+R12)./R15 + (-3*x.^2+R22)./R25 + 6*z.*d1.*(-5*x.^2+R12)./R17);
+               D(2,:) = D(2,:) + C*3*( (-3 + 4*nu)*(x.*y)./R15 - (x.*y)./R25 - 10*x.*y.*z.*d1./R17);
+               D(3,:) = D(3,:) + C*3*( (-3 + 4*nu)*(x.*d1)./R15 - (x.*d2)./R25 - 2*x.*z.*(-5*d1.^2+R12)./R17);
+               D(4,:) = D(4,:) + C*3*( (-3 + 4*nu)*(x.*y)./R15 - (x.*y)./R25 - 10*x.*y.*z.*d1./R17);
+               D(5,:) = D(5,:) + C*( (3 - 4*nu)*(-3*y.^2+R12)./R15 + (-3*y.^2+R22)./R25 + 6*z.*d1.*(-5*y.^2+R12)./R17);
+               D(6,:) = D(6,:) + C*3*( (-3 + 4*nu)*(y.*d1)./R15 - (y.*d2)./R25 - 2*y.*z.*(-5*d1.^2+R12)./R17);
+               D(7,:) = D(7,:) + C*3*( (5 - 4*nu)*(x.*d1)./R15 - (x.*d2)./R25 - 2*x.*z.*(-5*d1.^2+R12)./R17);
+               D(8,:) = D(8,:) + C*3*( (5 - 4*nu)*(y.*d1)./R15 - (y.*d2)./R25 - 2*y.*z.*(-5*d1.^2+R12)./R17);
+               D(9,:) = D(9,:) + C*( (4*nu - 1)*(-3*d1.^2+R12)./R15 + (-3*d2.^2+R22)./R25 + 6*z.*d1.*(-5*d1.^2+3*R12)./R17);
 
-%Compute tilts (if necessary)
+          %Calculate stresses
 
-	if nargout > 2
-   	t(1,:)=-3*R5.*E*volgeom(3);
-   	t(2,:)=-3*R5.*N*volgeom(3);
-	end
+               theta = lambda*(D(1,:)+D(5,:)+D(9,:));
+
+               S(1,:) = S(1,:) + theta + 2*mu*D(1,:);
+               S(2,:) = S(2,:) + mu*(D(2,:)+D(4,:));
+               S(3,:) = S(3,:) + mu*(D(3,:)+D(7,:));
+               S(4,:) = S(4,:) + theta + 2*mu*D(5,:);
+               S(5,:) = S(5,:) + mu*(D(6,:)+D(8,:));
+               S(6,:) = S(6,:) + theta + 2*mu*D(9,:);
+
+   end
